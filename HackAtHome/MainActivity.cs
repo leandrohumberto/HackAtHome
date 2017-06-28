05ba2b564bcbc02879bd60d42efffdc962517089
Leandro Santos <leandro.santos@atsnet.com.br>
2017-06-28 17:34:16 -0300

    Chamada ao método da SAL para autenticação do usuário.

--- HackAtHome/MainActivity.cs
+++ HackAtHome/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using HackAtHome.Entities;
+using HackAtHome.SAL;
 
 namespace HackAtHome
 {
@@ -13,6 +15,33 @@ namespace HackAtHome
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            FindViewById<Button>(Resource.Id.buttonValidate).Click += async (sender, e) =>
+            {
+                var email = FindViewById<TextView>(Resource.Id.editTextEmail).Text;
+                var password = FindViewById<TextView>(Resource.Id.editTextPassword).Text;
+                ServiceClient client = new ServiceClient();
+                var result = await client.AutenticateAsync(email, password);
+
+                if (result.Status == Status.AllSuccess || result.Status == Status.Success)
+                {
+                    var dialog = new AlertDialog.Builder(this).Create();
+                    dialog.SetButton("OK", delegate { });
+                    dialog.SetMessage($"{result.Status}\n{result.FullName}\n{result.Token}");
+                    dialog.SetTitle("Done");
+                    dialog.SetIcon(Android.Resource.Drawable.PresenceOnline);
+                    dialog.Show();
+                }
+                else
+                {
+                    var dialog = new AlertDialog.Builder(this).Create();
+                    dialog.SetButton("OK", delegate { });
+                    dialog.SetMessage(result.Status.ToString());
+                    dialog.SetTitle("Error");
+                    dialog.SetIcon(Android.Resource.Drawable.StatNotifyError);
+                    dialog.Show();
+                }
+            };
         }
     }
 }
